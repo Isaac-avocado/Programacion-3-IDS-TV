ff80a8822b99d1747e22eea0ce617969a498ff5e
Isaac Alvarez <61669739+Isaac-avocado@users.noreply.github.com>
2024-02-22 14:43:54 -0700

    unidad 2: actividad 12 - Error de centrado

--- src/Ventana.java
+++ src/Ventana.java
@@ -1,5 +1,8 @@
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -14,6 +17,7 @@ public class Ventana extends JFrame{
 		private JPanel panel, registro, login;
 		private JTextField cajaTexto;
 		private JPasswordField cajaContrasena;
+		private GridBagConstraints gridBagConstraints = new GridBagConstraints();
 		public Ventana() {
 			setVisible(true);
 			setTitle("Login window");
@@ -30,12 +34,12 @@ public class Ventana extends JFrame{
 			panelRegistro();
 			String dato;
 			int x,y,h,w;
-			etiquetaDeTexto(dato="SISTEMA P.A.C.S.",125,30,200,30, login);
-			etiquetaDeTexto(dato="Token",140,100,200,30, login);
-			etiquetaDeTexto(dato="Contraseña",132,200,200,30, login);
-			botonAccion();
-			cajaDeTexto();
-			cajaDeContrasena();
+			etiquetaDeTexto(dato="SISTEMA P.A.C.S.",125,30,200,30, login,0);
+			etiquetaDeTexto(dato="Token",140,100,200,30, login,1);
+			etiquetaDeTexto(dato="Contraseña",132,200,200,30, login,3);
+			botonAccion(6);
+			cajaDeTexto(2);
+			cajaDeContrasena(4);
 			
 			this.revalidate();
 			this.repaint();
@@ -71,27 +75,47 @@ public class Ventana extends JFrame{
 			//etiqueta.setText("SISTEMA P.A.C.S.");
 			//panel.add(etiqueta);
 		}
-		private void etiquetaDeTexto(String dato,int x,int y, int h,int w, JPanel panel) {
+		private void etiquetaDeTexto(String dato,int x,int y, int h,int w, JPanel panel, int gridNumero) {
 			JLabel etiqueta = new JLabel();
 			etiqueta.setText(dato);
-			etiqueta.setBounds(x,y,h,w);
-			panel.add(etiqueta);
+			//etiqueta.setBounds(x,y,h,w);
+			panel.setLayout(new GridBagLayout());
+			gridBagConstraints.gridy = gridNumero;
+			gridBagConstraints.weightx = 0.4;
+			gridBagConstraints.weighty = gridNumero;
+			//gridBagConstraints.insets = new Insets(5, 5, 5, 5);
+			panel.add((etiqueta), gridBagConstraints);
 		}
-		private void cajaDeTexto() {
+		private void cajaDeTexto(int gridNumero) {
 			JTextField cajaTexto = new JTextField();
-			cajaTexto.setBounds(100,130,200,30);
+			//cajaTexto.setBounds(100,130,200,30);
 			//cajaTexto.setText("Hola.");
+			panel.setLayout(new GridBagLayout());
+			gridBagConstraints.gridy = gridNumero;
+			gridBagConstraints.weightx = 0.4;
+			gridBagConstraints.weighty = 0.4;
+			//gridBagConstraints.insets = new Insets(5, 5, 5, 5);
 			login.add(cajaTexto);
 		}
-		private void cajaDeContrasena() {
+		private void cajaDeContrasena(int gridNumero) {
 			JPasswordField cajaContrasena = new JPasswordField();
-			cajaContrasena.setBounds(100,230,200,30);
+			//cajaContrasena.setBounds(100,230,200,30);
+			panel.setLayout(new GridBagLayout());
+			gridBagConstraints.gridy = gridNumero;
+			gridBagConstraints.weightx = 0.4;
+			gridBagConstraints.weighty = 0.4;
+			//gridBagConstraints.insets = new Insets(5, 5, 5, 5);
 			login.add(cajaContrasena);
 		}
-		private void botonAccion() {
+		private void botonAccion(int gridNumero) {
 			JButton boton = new JButton("ENTRAR");
-			boton.setBounds(125,300,100,30);
+			//boton.setBounds(125,300,100,30);
 			//boton.addActionListener((ActionListener) this);
+			panel.setLayout(new GridBagLayout());
+			gridBagConstraints.gridy = gridNumero;
+			gridBagConstraints.weightx = 0.4;
+			/gridBagConstraints.weighty = 0.4;
+			//gridBagConstraints.insets = new Insets(5, 5, 5, 5);
 			login.add(boton);
 		}
 		@SuppressWarnings("unused")
